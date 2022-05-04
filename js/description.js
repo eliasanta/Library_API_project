811426b7fee3c53ce01d4b8c16f0ab8c937364b1
eliasanta <eliasanta93@gmail.com>
2022-05-04 16:08:42 +0200

    fixed filter

--- js/description.js
+++ js/description.js
@@ -4,13 +4,12 @@ class Description{
        // this.comments = new Comments();
     }
     async showPostByUserKey(key){ 
-        console.log(key)
         document.getElementsByClassName("spinner-border")[0].style.display = "block";
         try{
             const url = `https://openlibrary.org${key}.json`;
-            console.log(url)
+            
             this.data = await api.getFetch(url);  
-            console.log(this.data)          
+                    
             this.showModal();
         }
         catch(e){
@@ -23,11 +22,11 @@ class Description{
     showModal(){
         /* anche in caso non trovi un adescription o sia su description.value la visualizza */
         if(this.data.description == null || this.data.description == undefined){
-            console.log('trovato undefined')
+            
             document.getElementById("modal-body").innerText="Non vi è alcuna descrizione"
         }else {
             this.data.description.value != null ? document.getElementById("modal-body").innerText = this.data.description.value : document.getElementById("modal-body").innerText = this.data.description
-            console.log('entro nel secondoperche la prima è falsa')
+          
         }
         
        //document.getElementById("postDiv").style.display = "block";
--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-     
 //variabili const. Queste sono le URL delle API e non devono essere modificate
 
-const URL_DESCRIPTION="https://openlibrary.org"
+const URL_DESCRIPTION = "https://openlibrary.org";
 const URL_USERS = "https://jsonplaceholder.typicode.com/users";
 const URL_PHOTOS = "https://jsonplaceholder.typicode.com/photos";
 const URL_TODOS = "https://jsonplaceholder.typicode.com/todos";
@@ -9,113 +8,108 @@ const URL_ALBUMS = "https://jsonplaceholder.typicode.com/albums";
 const URL_POST = "https://jsonplaceholder.typicode.com/posts";
 const URL_COMMENTS = "https://jsonplaceholder.typicode.com/comments";
 
-
 var api = new Request();
-var post = new Posts();//creo array vuoti
-var desc =new Description();
+var post = new Posts(); //creo array vuoti
+var desc = new Description();
 var albums = new Albums();
 var todos = new Todo();
 
+let ListaLibri = []; //dove inserirò libri filtrati e non
+
 //**************************************************************for sidebar */
-let ListaLibri = []; //creo variabile lista libri
+
 $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+  });
 });
 //*************************************************************************** */
 //************************************imposto un piccolo ritardo in quanto devono prima caricarsi gli script */
-const myTimeout = setTimeout(()=>{
-    document.getElementsByClassName("spinner-border")[0].style.display = "none";
-    let btn=document.getElementById('getListId');
-    btn.addEventListener('click',function () {
-        let targetValue = document.getElementById("subList").value;
-        //in base a quello selezionato in lista carico i libri
-        recuperaUtenti(targetValue);
-      });
-}, 100);
+setTimeout(() => {
+  document.getElementsByClassName("spinner-border")[0].style.display = "none";
+  let btn = document.getElementById("getListId");
+  btn.addEventListener("click", function () {
+    let targetValue = document.getElementById("subList").value;
+    //in base a quello selezionato in lista carico i libri
+    recuperaUtenti(targetValue);
+  });
+}, 200);
 
-
-/*  window.addEventListener("load", function(){
-    recuperaUtenti();
-})  */
 /**
  * mostra il messaggio di errore nell'HTML
- * @param {string} message 
+ * @param {string} message
  */
- function showMessageError(message) {
-    document.getElementById("errorTxt").textContent = message;
-    document.getElementById("errorTxt").style.display = "block";
+function showMessageError(message) {
+  document.getElementById("errorTxt").textContent = message;
+  document.getElementById("errorTxt").style.display = "block";
 }
 /**
  * nasconde il messaggio di errore nell'HTML
  */
 function hideMessageError() {
-    document.getElementById("errorTxt").style.display = none;
+  document.getElementById("errorTxt").style.display = none;
 }
 
-function searchTitle(){
-    const srcTitle = document.getElementById('searchTitle');
-    //********************funzione per filtrare la ricerca */
-     srcTitle.addEventListener("keyup", (e) => {
-      const SearchString = e.target.value;
-      console.log(SearchString)
-      const filteredBooks = ListaLibri.filter((book) => {
-        return book.title.includes(SearchString);
-      });
-      printBooks(filteredBooks);
+
+
+setTimeout(() => {
+  const srcTitle = document.getElementById("searchTitle");
+  //********************funzione per filtrare la ricerca */
+  srcTitle.addEventListener("keyup", (e) => {
+    const SearchString = e.target.value.toLowerCase();
+    console.log(SearchString);
+    const filteredBooks = ListaLibri.works.filter((book) => {
+      return book.title.toLowerCase().includes(SearchString);
     });
+
+    printBooks(filteredBooks);
+  });
+}, 100);
+
+async function recuperaUtenti(subject) {
+  const URL_LIBRI = `https://openlibrary.org/subjects/${subject}.json`;
+  document.getElementsByClassName("spinner-border")[0].style.display = "block"; // visualizza lo spinner che viene disattivato alla conclusione della chiamata. Riga 44
+  try {
+    ListaLibri = await api.getFetch(URL_LIBRI);
+    printBooks(ListaLibri.works);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    document.getElementsByClassName("spinner-border")[0].style.display = "none";
+  }
 }
 
- 
-async function recuperaUtenti(subject){
-    console.log(subject)
-    const URL_LIBRI =`https://openlibrary.org/subjects/${subject}.json`;
-    document.getElementsByClassName("spinner-border")[0].style.display = "block"; // visualizza lo spinner che viene disattivato alla conclusione della chiamata. Riga 44
-    try{
-        let libri = await api.getFetch(URL_LIBRI);
-        printBooks(libri.works);
-        searchTitle();
-        
-    }
-    catch(e){
-        console.log(e);
-    }
-    finally{
-        document.getElementsByClassName("spinner-border")[0].style.display = "none";
-    }
-}
+function printBooks(elemResp) {
+  //passo oggetto completo fantasy
 
+  let str = `<h2 class="mb-3">Lista libri</h2>`;
 
-function printBooks(elemResp){//passo oggetto completo fantasy
-    
-    ListaLibri = elemResp;
-    let str = `<h2 class="mb-3">Lista libri</h2>`;
-    
-    ListaLibri.forEach((elem) => {//lista libri completa
-      str += `<div class="card card-body mb-2 mt-2">
-                    <h3>${elem.title}</h3>
-                    <p>elenco autori</p>
-                    <ul id="listAuthor">
-                    ${elem.authors.map((ele) => `<li >${ele.name}</li>`)}
-                    </ul>
+  elemResp.forEach((elem) => {
+    //lista libri completa
+
+    //gli autori li limito al primo
+    str += `<div class="card card-body mb-3 ">
+                    <h4 class="mt-1">${elem.title}</h4>
+                    <h6 class="pr-6">Autore:</h6>
+                        <div class="Lista Autori mb-3" >
+                        ${elem.authors
+                          .map((ele) => `<span> ${ele.name}</span>`)
+                          .slice(0, 1)}
+                        </div>
                     <div>
-                    <button class='btn btn-success' data-bs-toggle="modal" data-bs-target="#modalDesc" onclick='showDescription("${elem.key}")'>DESCRIPTION_test</button>
+                    <button class='btn btn-success' data-bs-toggle="modal" data-bs-target="#modalDesc" onclick='showDescription("${
+                      elem.key
+                    }")'>Book Description</button>
                     </div>
-             </div> `;                 
-    });
-    
-    document.getElementById("output").innerHTML = str;
-    }
+             </div> `;
+  });
 
-   
-   
+  document.getElementById("output").innerHTML = str;
+}
 
 //creo la funzione per mostrarmi la descrizione
- function showDescription(userKey) {//gli ho passato la chiave
-    document.getElementsByClassName("spinner-border")[0].style.display = "block";
-    desc.showPostByUserKey(userKey);//gli passo la chiave
-} 
-
-
-
+function showDescription(userKey) {
+  //gli ho passato la chiave
+  document.getElementsByClassName("spinner-border")[0].style.display = "block";
+  desc.showPostByUserKey(userKey); //gli passo la chiave
+}
